67f08773b2402dbc11acde1f589803de73c431f4
shin <nhathanh024@gmail.com>
2024-07-05 14:09:37 +0700

    init code

---
+++ src/wp-content/themes/agrikole-child/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+/*
+ * Define Variables
+ */
+if (!defined('THEME_DIR'))
+    define('THEME_DIR', get_template_directory());
+if (!defined('THEME_URL'))
+    define('THEME_URL', get_template_directory_uri());
+
+
+/*
+ * Include framework files
+ */
+foreach (glob(THEME_DIR.'-child' . "/includes/*.php") as $file_name) {
+    require_once ( $file_name );
+}
+
